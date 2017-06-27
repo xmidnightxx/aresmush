a8a0c1eeb114054b7c2d6bdc544470564d0e0a99
Scott Key <mscottkey@gmail.com>
2017-06-26 20:01:15 -0500

    Updating master.

--- game/plugins/website/web/public/config.js
+++ game/plugins/website/web/public/config.js
@@ -1,1 +1,1 @@
-var config = {"port":8005,"mu_name":"After Camelot: An Arthurian Legend","host":"scrozzle.noderunner.net"};
+var config = {"port":8005,"mu_name":"The 8th Sea","host":"scrozzle.noderunner.net"};
