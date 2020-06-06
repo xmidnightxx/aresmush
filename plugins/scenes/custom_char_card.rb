fe1eea9892ef3bd6a2c17a57e860537b12d142ae
xmidnightxx <64651054+xmidnightxx@users.noreply.github.com>
2020-06-06 11:42:28 -0400

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -3,11 +3,8 @@ module AresMUSH
     def self.custom_char_card_fields(char, viewer)
   {
     traits = (char.traits || {}).map { |k, v| {name: k, description: v }
-    char.traits.map => traits
   }
-  {
-    traits: Website.format_markdown_for_html(traits)
-  }
+
     end
   end
 end
