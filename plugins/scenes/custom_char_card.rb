111b5a1156d367c5fafff5b3dafa19db37162492
xmidnightxx <64651054+xmidnightxx@users.noreply.github.com>
2020-06-06 11:49:00 -0400

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -5,7 +5,7 @@ self.custom_char_card_fields(char, viewer)
   traits = (char.traits || {}).map { |k, v| {name: k, description: v } 
     }
  {
-        traits: Website.format_markdown_for_html(traits)
+        traits: "traits"
  }
    
 end
