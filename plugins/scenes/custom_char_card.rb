648c6e0889ebfcfae541ef150544287e597a754b
xmidnightxx <64651054+xmidnightxx@users.noreply.github.com>
2020-06-05 22:03:28 -0400

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -2,18 +2,9 @@ module AresMUSH
   module Scenes
     
     def self.custom_char_card_fields(char, viewer)
-      
-      # Return nil if you don't need any custom fields.
-      {
-        traits = (char.traits || {}).map { |k, v| {name: k, description: v } }
-      }
-      {
-        traits: Website.format_markdown_for_html(traits):
-      }
-      # Otherwise return a hash of data.  For example, if you want to show traits you could do:
-      # {
-      #   traits: char.traits.map { |k, v| { name: k, description: v } }
-      # }
+traits = (char.traits || {}).map { |k, v| {name: k, description: v } 
+{
+        traits: Website.format_markdown_for_html(traits):  }
     end
   end
 end
