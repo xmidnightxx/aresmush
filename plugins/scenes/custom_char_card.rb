0786f55df2c624dfbf38df948a8d48325a274888
xmidnightxx <64651054+xmidnightxx@users.noreply.github.com>
2020-06-06 13:49:17 -0400

    Update custom_char_card.rb

--- plugins/scenes/custom_char_card.rb
+++ plugins/scenes/custom_char_card.rb
@@ -3,7 +3,7 @@ module AresMUSH
     def self.custom_char_card_fields(char, viewer)
        # This sets a temporary variable named 'traits' with
        # includes the name and description
-       traits=(char.traits || {}).map { |k, v| {name: k, description: Website.format_markdown_for_html(v)}
+       traits=(char.traits || {}).map { |k, v| {name: k, description: Website.format_markdown_for_html(v)}}
        {traits: traits}
     end
   end
